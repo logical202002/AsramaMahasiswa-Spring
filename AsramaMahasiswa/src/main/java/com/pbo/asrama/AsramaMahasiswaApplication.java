ca5dd57074e5c11a75701b57663214b8363810af
ff.paiki <logical202002@gmail.com>
2020-04-25 22:45:44 +0900

    Bootstrap Enabled

---
+++ AsramaMahasiswa/src/main/java/com/pbo/asrama/AsramaMahasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.pbo.asrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsramaMahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsramaMahasiswaApplication.class, args);
+	}
+
+}
---
+++ AsramaMahasiswa/src/test/java/com/pbo/asrama/AsramaMahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbo.asrama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsramaMahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
